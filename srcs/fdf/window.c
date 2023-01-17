415b81a34d060483e258c9cd0ee0515f2418958b
Oussama BOUHLEL <obouhlel@e1r1p4.clusters.42paris.fr>
2023-01-17 10:45:02 +0100

    Secure minilibx

--- srcs/fdf/window.c
+++ srcs/fdf/window.c
@@ -6,7 +6,7 @@
 /*   By: obouhlel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/25 17:57:11 by obouhlel          #+#    #+#             */
-/*   Updated: 2023/01/16 15:18:32 by obouhlel         ###   ########.fr       */
+/*   Updated: 2023/01/17 10:43:44 by obouhlel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,10 +52,18 @@ void	put_pixel(t_vars *vars, int x, int y, int color)
 int	window_init(t_vars *vars)
 {
 	vars->mlx = mlx_init();
+	if (!vars->mlx)
+		return (EXIT_FAILURE);
 	vars->win = mlx_new_window(vars->mlx, WIN_X, WIN_Y, "FDF obouhlel");
+	if (!vars->win)
+		return (EXIT_FAILURE);
 	vars->img = mlx_new_image(vars->mlx, WIN_X, WIN_Y);
+	if (!vars->img)
+		return (EXIT_FAILURE);
 	vars->addr = mlx_get_data_addr(vars->img, &(vars->bit_per_pixel), \
 									&(vars->size_line), &(vars->endian));
+	if (!vars->addr)
+		return (EXIT_FAILURE);
 	ft_trace_img(vars);
 	mlx_put_image_to_window(vars->mlx, vars->win, vars->img, 0, 0);
 	mlx_key_hook(vars->win, &keyboard, vars);
